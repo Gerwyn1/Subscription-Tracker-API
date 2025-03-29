44624f8cc098464166a094fd585ad011d416a7ea
gerwyn <gerwynsim@hotmail.com>
2025-03-29 16:35:47 +0800

    authentication JWT

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser)
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, async () => {
   console.log(
-    `Subscription Tracker API is running on https://localhost:${PORT}`
+    `Subscription Tracker API is running on http://localhost:${PORT}`
   );
 
   await connectToDatabase();
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({ title: "sign up" });
-});
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "sign in" });
-});
-authRouter.post("/sign-out", (req, res) => {
-  res.send({ title: "sign out" });
-});
+authRouter.post("/sign-up", signup);
+authRouter.post("/sign-in", signin);
+authRouter.post("/sign-out", () => {});
 
 export default authRouter;
